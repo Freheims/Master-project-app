557daf6d4461917d5530081e0a621456440178c4
Taco Babe <tacobabe@eduroam-nat-64.eduroam.uib.no>
2018-03-12 16:22:05 +0100

    added list of all sessions

---
+++ app/src/main/java/no/uib/master_project_app/SessionListActivity.java
@@ -0,0 +1,13 @@
+package no.uib.master_project_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SessionListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_session_list);
+    }
+}
---
+++ app/src/main/java/no/uib/master_project_app/adapters/SessionListViewAdapter.java
@@ -0,0 +1,8 @@
+package no.uib.master_project_app.adapters;
+
+/**
+ * Created by tacobabe on 12.03.2018.
+ */
+
+public class SessionListViewAdapter {
+}
