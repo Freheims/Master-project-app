7206d00f1760843143a999465cd5fdbae40f7bef
Fredrik Heimsæter <freheims@gmail.com>
2018-11-26 16:00:12 +0100

    Fixed typos

--- app/src/main/java/no/uib/master_project_app/TrackingActivity.java
+++ app/src/main/java/no/uib/master_project_app/TrackingActivity.java
@@ -653,7 +653,7 @@ public class TrackingActivity extends AppCompatActivity implements Accelerometer
                 countdown = 22;
             if((Math.abs(prevY - gravity[1]) > threshold) && !ignore){
                 steps++;
-                textViewTrackingStatus.setText(getString(R.string.stepcount) + steps);
+                textViewTrackingStatus.setText(getString(R.string.stepcount) + " " + steps);
 
                 ignore = true;
             }
